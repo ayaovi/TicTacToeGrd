c0b98cdc93917ef654727af6493047f559f96cdf
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-12-03 20:44:40 +0200

    login through firebase.

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -5,19 +5,31 @@
 		.module('app')
 		.controller('LoginController', LoginController);
 
-	LoginController.$inject = ['$location'];
-	function LoginController($location) {
+	LoginController.$inject = ['$scope', '$location', '$cookies', '$rootScope'];
+	function LoginController($scope, $location, $cookies, $rootScope) {
 		var vm = this;
 
 		vm.login = login;
 
 		(function initController() {
 			// reset login status
+			$rootScope.globals = {};
+			$cookies.remove('globals');
 			// AuthenticationService.ClearCredentials();
 		})();
 
 		function login() {
 			vm.dataLoading = true;
+			firebase.auth().signInWithEmailAndPassword(vm.username, vm.password)
+			.then(function () {
+				console.log('Successful Login!');
+				const user = firebase.auth().currentUser;
+				//log user in.
+			})
+			.catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+			});
 		};
 	}
 })();
