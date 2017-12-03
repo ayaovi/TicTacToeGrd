66fdd83ebf1df0e84f9aff86e0c55ec3f1dc3bf6
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-12-03 20:35:00 +0200

    register through firebase.

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -5,29 +5,19 @@
 		.module('app')
 		.controller('LoginController', LoginController);
 
-	LoginController.$inject = ['$location', 'AuthenticationService', 'UserService', 'FlashService'];
-	function LoginController($location, AuthenticationService, UserService, FlashService) {
+	LoginController.$inject = ['$location'];
+	function LoginController($location) {
 		var vm = this;
 
 		vm.login = login;
 
 		(function initController() {
 			// reset login status
-			AuthenticationService.ClearCredentials();
+			// AuthenticationService.ClearCredentials();
 		})();
 
 		function login() {
 			vm.dataLoading = true;
-			UserService.Authenticate(vm.username, vm.password)
-				.then(function (response) {
-					if (response.success) {
-						AuthenticationService.SetCredentials(vm.username, vm.password);
-						$location.path('/');
-					} else {
-						FlashService.Error(response.message);
-						vm.dataLoading = false;
-					}
-				});
 		};
 	}
 })();
--- src/register/register.controller.js
+++ src/register/register.controller.js
@@ -5,25 +5,25 @@
 		.module('app')
 		.controller('RegisterController', RegisterController);
 
-	RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-	function RegisterController(UserService, $location, $rootScope, FlashService) {
+	RegisterController.$inject = ['$scope', '$location', 'FlashService'];
+	function RegisterController($scope, $location, FlashService) {
 		var vm = this;
 
 		vm.register = register;
 
 		function register() {
 			vm.dataLoading = true;
-			UserService.Create(vm.user)
-				.then(function (response) {
-					if (response.success) {
-						FlashService.Success('Registration successful', true);
-						$location.path('/login');
-					} else {
-						FlashService.Error(response.message);
-						vm.dataLoading = false;
-					}
-				});
+			firebase.auth().createUserWithEmailAndPassword(vm.user.username, vm.user.password)
+			.then(function () {
+				// const user = firebase.auth().currentUser;
+				// console.log("Successful Registration!");
+				$location.path('/login').replace();
+				$scope.$apply();
+			})
+			.catch(function(error) {
+				FlashService.Error(error.message);
+				vm.dataLoading = false;
+			});
 		}
 	}
-
 })();
